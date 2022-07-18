5bf7cc5b6649069962d78e2f622c0c921714c8b8
inrir <kimms5617@naver.com>
2022-07-19 00:14:11 +0900

    220718 1 solving

---
+++ src/Week2/B11654.java
@@ -0,0 +1,14 @@
+package Week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        char a = (char)bf.read();
+        System.out.println((int)a);
+    }
+}
